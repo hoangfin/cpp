3023f4108f598b1ca3c9b542c3309bd09e75dfe5
hoangfin <hoang.tran.fin@gmail.com>
2025-05-15 00:21:56 +0300

    Updated

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -4,14 +4,16 @@
 int main(int argc, char **argv) {
 	if (argc != 2) {
 		std::cerr << "Error: could not open file." << std::endl;
+		return 1;
 	}
-	
+
 	try {
 		BitcoinExchange btcExchange;
 
 		btcExchange.display(argv[1]);
 	} catch(const std::exception& e) {
 		std::cerr << e.what() << '\n';
+		return 1;
 	}
 
 	return 0;
